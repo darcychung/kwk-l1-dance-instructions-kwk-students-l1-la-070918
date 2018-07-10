674f7eb2d1dfb171ab62f98bef0a48c5032918e3
darcychung <darcycchung@gmail.com>
2018-07-10 05:36:33 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,43 @@
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def base_footwork
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+end
+def skip_step_left
+  puts "lower left foot to the ground"
+  puts "bounce left foot back up slightly, kicking it a few inches back"
+  puts "left, right, left, right"
+end
+def skip_step_right
+  puts "lower right foot to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts "right, left, right and right"
+end
+def bob_the_reins
+  puts "holding your arms in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and dowm in a loose bouncing movement, in time with the beat of the song. this movement is repeated eight times"
+end
+def lasso 
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+  puts "lift right arm up and point right elbow diagonally to the right"
+  puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. this movement is repeated eight times as well"
+end
+def bust_a_move
+  puts "be cool"
+  puts "be snazzy"
+end
+def gangnam_style
+  puts "assume stance"
+  puts "bust a move"
+  puts "footwork"
+  puts "bob the reins + skip-steps x8"
+  puts "lasso +skip-steps x8"
+end
 
+dance_instructions.rb 
 # Code your methods below
+
